67498c75e6dfb42cad3f276d1694304d63c0a807
kga-tech <kga.tech@lidera.agency>
2024-04-20 23:28:13 +0300

    fix & docker compose

--- schema.sql
+++ schema.sql
@@ -1,7 +1,20 @@
+DO
+$do$
+BEGIN
+   IF EXISTS (SELECT FROM pg_database WHERE datname = 'db-model-test-job') THEN
+      RAISE NOTICE 'Database already exists';
+   ELSE
+      PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE mydb');
+   END IF;
+END
+$do$;
+
 CREATE TABLE IF NOT EXISTS model (
     Id VARCHAR(128) PRIMARY KEY NOT NULL,
     Name VARCHAR(128) NOT NULL,
     Description TEXT NOT NULL,
     Context_length INTEGER,
     Tokenizer VARCHAR(64)
-)
+);
+
+
